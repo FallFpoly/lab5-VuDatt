4755363c944299c23799c0648b6e245b8e0329ff
vudat <vudat@vudat>
2025-10-16 04:45:55 +0700

    nop lap 5

--- Lab5/src/main/java/com/mycompany/lab5/bai2.java
+++ Lab5/src/main/java/com/mycompany/lab5/bai2.java
@@ -1,16 +1,113 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.lab5;
 
-/**
- *
- * @author ICT
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class bai2 {
 
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int chon;
+        do {
+            menu();
+            System.out.print("Chon chuc nang: ");
+            chon = sc.nextInt();
+            sc.nextLine();
+            switch (chon) {
+                case 1:
+                    nhap();
+                    break;
+                case 2:
+                    xuat();
+                    break;
+                case 3:
+                    ngaunhien();
+                    break;
+                case 4:
+                    sapxep();
+                    break;
+                case 5:
+                    xoa();
+                    break;
+                case 6:
+                    System.out.println("Ket thuc chuong trinh.");
+                    break;
+                default:
+                    System.out.println("Vui long chon tu 1 den 6.");
+            }
+        } while (chon != 6);
+    }
+
+    public static void menu() {
+        System.out.println("\n------ MENU ------");
+        System.out.println("1. Nhap danh sach ho ten");
+        System.out.println("2. Xuat danh sach vua nhap");
+        System.out.println("3. Xuat danh sach ngau nhien");
+        System.out.println("4. Sap xep giam dan va xuat danh sach");
+        System.out.println("5. Tim va xoa ho ten");
+        System.out.println("6. Ket thuc");
+    }
+
+    public static void nhap() {
+        while (true) {
+            System.out.print("Nhap ho ten: ");
+            String hoTen = sc.nextLine();
+            list.add(hoTen);
+            System.out.print("Nhap them (Y/N)? ");
+            String tiepTuc = sc.nextLine();
+            if (tiepTuc.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+    }
+
+    public static void xuat() {
+        System.out.println("\nDanh sach ho ten:");
+        for (String ten : list) {
+            System.out.println(ten);
+        }
+    }
+
+    public static void ngaunhien() {
+        ArrayList<String> copyList = new ArrayList<>(list);
+        Collections.shuffle(copyList);
+        System.out.println("\nDanh sach ngau nhien:");
+        for (String ten : copyList) {
+            System.out.println(ten);
+        }
+    }
+
+    public static void sapxep() {
+        Collections.sort(list);              
+        Collections.reverse(list);            
+        System.out.println("\nDanh sach sap xep giam dan:");
+        for (String ten : list) {
+            System.out.println(ten);
+        }
+    }
+
+    public static void xoa() {
+        System.out.print("Nhap ho ten can xoa: ");
+        String tenCanXoa = sc.nextLine();
+        boolean timThay = false;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equalsIgnoreCase(tenCanXoa)) {
+                list.remove(i);
+                timThay = true;
+                System.out.println("Da xoa ho ten: " + tenCanXoa);
+                break;
+            }
+        }
+
+        if (!timThay) {
+            System.out.println("Khong tim thay ho ten: " + tenCanXoa);
+        }
     }
 }
+
--- Lab5/src/main/java/com/mycompany/lab5/bai3.java
+++ Lab5/src/main/java/com/mycompany/lab5/bai3.java
@@ -1,16 +1,133 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.lab5;
 
-/**
- *
- * @author ICT
- */
-public class bai3 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class SanPham {
+    String ten;
+    Double donGia;
+
+    public SanPham(String ten, Double donGia) {
+        this.ten = ten;
+        this.donGia = donGia;
+    }
+
+    public String toString() {
+        return "Ten: " + ten + ", Gia: " + donGia;
     }
 }
+
+public class bai3 {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<SanPham> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int chon;
+        do {
+            menu();
+            System.out.print("Chon chuc nang: ");
+            chon = Integer.parseInt(sc.nextLine());
+
+            switch (chon) {
+                case 1:
+                    nhap();
+                    break;
+                case 2:
+                    sapXepVaXuat();
+                    break;
+                case 3:
+                    xoa();
+                    break;
+                case 4:
+                    xuatTrungBinh();
+                    break;
+                case 5:
+                    System.out.println("Ket thuc chuong trinh.");
+                    break;
+                default:
+                    System.out.println("Vui long chon tu 1 den 5.");
+            }
+        } while (chon != 5);
+    }
+
+    public static void menu() {
+        System.out.println("\n------ MENU ------");
+        System.out.println("1. Nhap danh sach san pham");
+        System.out.println("2. Sap xep giam dan theo gia va xuat danh sach");
+        System.out.println("3. Tim va xoa san pham theo ten");
+        System.out.println("4. Xuat gia trung binh cua cac san pham");
+        System.out.println("5. Ket thuc");
+    }
+
+    public static void nhap() {
+        while (true) {
+            System.out.print("Nhap ten san pham: ");
+            String ten = sc.nextLine();
+            System.out.print("Nhap don gia: ");
+            Double gia = Double.parseDouble(sc.nextLine());
+            list.add(new SanPham(ten, gia));
+
+            System.out.print("Nhap them (Y/N)? ");
+            String tiepTuc = sc.nextLine();
+            if (tiepTuc.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+    }
+
+    public static void sapXepVaXuat() {
+        Comparator<SanPham> comp = new Comparator<SanPham>() {
+            @Override
+            public int compare(SanPham o1, SanPham o2) {
+                return o2.donGia.compareTo(o1.donGia); 
+            }
+        };
+
+        Collections.sort(list, comp);
+
+        System.out.println("\nDanh sach san pham sap xep giam dan theo gia:");
+        for (SanPham sp : list) {
+            System.out.println(sp);
+        }
+    }
+
+    public static void xoa() {
+        System.out.print("Nhap ten san pham can xoa: ");
+        String tenXoa = sc.nextLine();
+        boolean timThay = false;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).ten.equalsIgnoreCase(tenXoa)) {
+                list.remove(i);
+                System.out.println("Da xoa san pham: " + tenXoa);
+                timThay = true;
+                break;
+            }
+        }
+
+        if (!timThay) {
+            System.out.println("Khong tim thay san pham co ten: " + tenXoa);
+        }
+    }
+
+    public static void xuatTrungBinh() {
+        if (list.isEmpty()) {
+            System.out.println("Khong co san pham nao de tinh trung binh.");
+            return;
+        }
+
+        double tong = 0;
+        for (SanPham sp : list) {
+            tong += sp.donGia;
+        }
+
+        double trungBinh = tong / list.size();
+        System.out.println("Gia trung binh cua cac san pham: " + trungBinh);
+    }
+}
+
